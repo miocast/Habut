104ebcb67d1e501a73382826469a2baabdcf5faa
miocast <rinatsoi05@gmail.com>
2023-04-23 19:57:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/habut/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.habut
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.habut.ui.theme.HabutTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ListItem(name = "")
+        }
+    }
+    @Composable
+    private  fun ListItem(name: String){
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            shape = RoundedCornerShape(15.dp),
+            elevation = 3.dp
+        ) {
+            Box {
+                Column {
+                    Image(
+                        painter = painterResource(id = R.drawable.icon_star),
+                        contentDescription = "icon_star",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .padding(5.dp)
+                            .size(64.dp)
+                            .clip(CircleShape))
+                }
+            }
+        }
+    }
+}
+
+
+
