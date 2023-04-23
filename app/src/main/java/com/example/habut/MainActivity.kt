b98841577fe12918ee1e9e48e624666db6af2c76
miocast <rinatsoi05@gmail.com>
2023-04-23 20:35:57 +0300

    Учусь делать коммиты, начало проекта

--- app/src/main/java/com/example/habut/MainActivity.kt
+++ app/src/main/java/com/example/habut/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.habut
 
+import android.graphics.fonts.FontFamily
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -16,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,34 +26,39 @@ import com.example.habut.ui.theme.HabutTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //данные из базы данных подключать
         setContent {
-            ListItem(name = "")
+            ListItem(name = "Название")
         }
     }
     @Composable
     private  fun ListItem(name: String){
         Card(
             modifier = Modifier
+                .height(95.dp)
                 .fillMaxWidth()
-                .padding(10.dp),
-            shape = RoundedCornerShape(15.dp),
+                .padding(16.dp),
+            shape = RoundedCornerShape(100.dp),
             elevation = 3.dp
         ) {
             Box {
-                Column {
+                Row (verticalAlignment = Alignment.CenterVertically){
                     Image(
-                        painter = painterResource(id = R.drawable.icon_star),
-                        contentDescription = "icon_star",
+                        painter = painterResource(id = R.drawable.imn),
+                        contentDescription = "imn",
                         contentScale = ContentScale.Crop,
                         modifier = Modifier
                             .padding(5.dp)
-                            .size(64.dp)
-                            .clip(CircleShape))
+                            .size(55.dp)
+                            .clip(CircleShape)
+                    )
+                    Text(text = name, fontFamily = FontFamily.Comfortaa)
+                }
                 }
             }
         }
     }
-}
+
 
 
 
