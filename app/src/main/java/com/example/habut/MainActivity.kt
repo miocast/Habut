9804399146efd1faf3f1a64fc886edd587fa49d6
miocast <rinatsoi05@gmail.com>
2023-04-28 10:35:57 +0300

    градиент фона, текст

--- app/src/main/java/com/example/habut/MainActivity.kt
+++ app/src/main/java/com/example/habut/MainActivity.kt
@@ -5,73 +5,105 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.LinearGradient
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.habut.ui.theme.Cosmos100
 import com.example.habut.ui.theme.HabutTheme
+import com.example.habut.ui.theme.Violet100
+import com.example.habut.ui.theme.comfortaa
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //данные из базы данных подключать
+
         setContent {
-            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+            HabutTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    VerticalGradient()
+                }
+            }
+            Column(modifier = Modifier
+                .verticalScroll(rememberScrollState())) {
+                Text(text = "Твои привычки",
+                    modifier = Modifier.padding(start = 67.dp, top = 106.dp,bottom = 45.dp),
+                    fontFamily = comfortaa,
+                    color = Color.White,
+                    textAlign = TextAlign.Center,
+                    fontSize = 30.sp)
+
                 ListItem(name = "Название")
                 ListItem(name = "Название")
-                ListItem(name = "Название")
-                ListItem(name = "Название")
-                ListItem(name = "Название")
-                ListItem(name = "Название")
-                ListItem(name = "Название")
-                ListItem(name = "Название")
-                ListItem(name = "Название")
-                ListItem(name = "Название")
-                ListItem(name = "Название")
+
             }
         }
     }
+
+    @Composable
+    fun VerticalGradient(){
+        val gradient = Brush.verticalGradient(
+            0.0f to Violet100,
+            1.0f to Cosmos100,
+            startY = 0.0f,
+            endY = 2800.0f
+        )
+        Box(modifier = Modifier.background(gradient))
+    }
+
+
     @Composable
     private  fun ListItem(name: String){
         Card(
             modifier = Modifier
                 .height(80.dp)
                 .fillMaxWidth()
-                .padding(start = 16.dp, end = 16.dp, top = 10.dp, bottom = 4.dp)
+                .padding(start = 22.dp, end = 22.dp, top = 10.dp, bottom = 4.dp)
                 .clickable {
                     Log.d("MyLog", "Жмакнули")
                 },
             shape = RoundedCornerShape(100.dp),
-            elevation = 3.dp
+            elevation = 5.dp
         ) {
             Box {
                 Row (verticalAlignment = Alignment.CenterVertically){
+
                     Image(
-                        painter = painterResource(id = R.drawable.imn),
-                        contentDescription = "imn",
+                        painter = painterResource(id = R.drawable.violet),
+                        contentDescription = "violet",
                         contentScale = ContentScale.Crop,
                         modifier = Modifier
-                            .padding(5.dp)
-                            .size(55.dp)
+                            .padding(8.dp)
+                            .size(50.dp)
                             .clip(CircleShape)
                     )
-                    Text(text = name, modifier = Modifier.padding(start = 30.dp))
+
+                    Text(text = name, color = Violet100,fontFamily = comfortaa, modifier = Modifier.padding(start = 30.dp))
                 }
                 }
             }
--- app/src/main/java/com/example/habut/ui/theme/Color.kt
+++ app/src/main/java/com/example/habut/ui/theme/Color.kt
@@ -3,6 +3,8 @@ package com.example.habut.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
+val Purple500 = Color(red = 0x8A, green = 0x76, blue = 0x9D, alpha = 0xFF)
+val Purple700 = Color(red = 0x8A, green = 0x76, blue = 0x9D, alpha = 0xFF)
 val Teal200 = Color(0xFF03DAC5)
+val Violet100 = Color(0xFF6C5E8F)
+val Cosmos100 = Color(0xFFFFD6D4)
